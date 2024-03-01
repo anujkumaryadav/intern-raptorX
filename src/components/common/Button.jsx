761ac5dcc7c465c5f05ebfda0436a54f0be2c52b
Anujkumar Yadav <anujkumaryadav.in@gmail.com>
2024-03-01 21:38:51 +0530

    button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -4,7 +4,7 @@ export default function Button() {
   return (
     <>
         <div className='pt-10 px-20 pb-24'>
-            <button className='px-6 py-2 bg-gradient-to-r from-[#16EE9D] to-[#08A0F3] text-black font-semibold rounded-md text-lg shadow-md hover:from-[#16EE9D] hover:to-[#08A0F3] transition-colors duration-300'>Apply Now</button>
+            <button className='px-6 py-2 bg-gradient-to-r from-[#16EE9D] to-[#08A0F3] text-black font-gilroy font-semibold rounded-md text-lg shadow-md hover:from-[#16EE9D] hover:to-[#08A0F3] transition-colors duration-300'>Apply Now</button>
         </div>
     </>
   )
